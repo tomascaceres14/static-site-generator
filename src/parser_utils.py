00350f090389522c2a3773afa1a664dc191095be
tomascaceres14 <tomialegriacaceres@gmail.com>
2024-08-28 18:00:17 +0200

    refactor

--- src/parser_utils.py
+++ src/parser_utils.py
@@ -1,14 +1,5 @@
 from htmlnode import LeafNode
-from textnode import TextNode
-
-textnode_types = {
-    "text_type_text": "text",
-    "text_type_bold": "bold",
-    "text_type_italic": "italic",
-    "text_type_code": "code",
-    "text_type_link": "link",
-    "text_type_image": "image",
-}
+from textnode import TextNode, textnode_types
 
 
 def text_node_to_html_node(text_node: TextNode):
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -11,6 +11,49 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         htmlnode1 = HTMLNode("a", "test node", [], {"href": "google.com"})
         self.assertEqual(htmlnode1.props_to_html(), ' href="google.com"')
+
+    def test_to_html(self):
+        node = LeafNode("a", "esto es un texto de prueba")
+        node2 = LeafNode("a", "vamos a google", {"href": "google.com"})
+        self.assertEqual(node.to_html(), "<a>esto es un texto de prueba</a>")
+        self.assertEqual(node2.to_html(), '<a href="google.com">vamos a google</a>')
+
+    def test_parent_basic(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        self.assertEqual(
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+            node.to_html(),
+        )
+
+    def test_parent_nested(self):
+        child_node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        node = ParentNode(
+            "div",
+            [child_node],
+        )
+
+        self.assertEqual(
+            "<div><p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p></div>",
+            node.to_html(),
+        )
 
 
 if __name__ == "__main__":
